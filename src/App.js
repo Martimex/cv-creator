3f117c715633d0fd315e9af7157aec5e0b0656eb
Martimex <kamilmarciniak533@wp.pl>
2021-06-26 15:28:46 +0200

    Add background, title, now work on buttons

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
 import logo from './logo.svg';
-import bgphoto_main from './images/cv-main.jpg';
-import bgphoto_left from  './images/cv-left.jpg';
-import bgphoto_right from './images/cv-right.jpg';
+import bgphoto_main from './images/cv-bg-main.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App"> 
 
-      <div className="bg">
-        <div className="layer-1" style={{ backgroundImage: `url(${bgphoto_main})`, backgroundRepeat: `no-repeat`, backgroundPosition: `center`, position: `relative`,  minHeight: `100vh`, width: `33%`,  }}>
-        </div>  
-        <div className="layer-2" style={{ backgroundImage: `url(${bgphoto_left})`, backgroundRepeat: `no-repeat`, backgroundPosition: `left`, position: `relative`,  minHeight: `100vh`, width: `33%`,  }}>      
-        </div> 
-        <div className="layer-3" style={{ backgroundImage: `url(${bgphoto_right})`, backgroundRepeat: `no-repeat`, backgroundPosition: `right`, position: `relative`,  minHeight: `100vh`, width: `33%`,  }}>   
-        </div> 
 
-      </div>
 
-      <div className="content" style={{ position: `absolute`, top: `0`, left: `50%`, marginLeft: `-400px`,  display: `block`}}>
+
+      <div className="layer" style={{ backgroundImage: `url(${bgphoto_main})`, backgroundRepeat: `no-repeat`, backgroundPosition: `center`, minHeight: `100vh`}}> 
+
         <header className="App-header">
           {/*<img src={logo} className="App-logo" alt="logo" />  KOMENTARZ */}
           <div className="main-title">
-            <div> Stwórz swoje wymarzone CV już dziś</div>
+            <span> Stwórz swoje wymarzone CV już dziś</span>
           </div>
         </header>
 
-        <div className="content">
+       {/*  <span class="welcome-text"> 
+          Kocham Kacperka. Curriculum Vitae to osobista wizytówka każdego kandydata. To od niej często zależy, czy dostaniesz 
+          wymarzoną pracę. Dla pracodawcy liczy się nie tylko treść, ale również i forma tego dokumentu. Siła pierwszego wrażenia pełni tu kluczową rolę.
+          </span>
+        */}
+
+        <div className="textbox">
+          <span className="welcome-text">
+            Nie wiesz jak stworzyć CV, którym oczarujesz pracodawcę? Świetnie trafiłeś! Wypróbuj nasz darmowy kreator, a przekonasz się, że pisanie CV 
+            może być proste, przyjemne, i - co najważniejsze - skuteczne.
+          </span>
+        </div>
+
+        <div className="btn-box">
 
           <button className="btn btn-start"> Start </button>
           <button className="btn btn-info"> Info </button>
           <button className="btn btn-prev"> Zanim zaczniesz </button>
         </div>
       </div>
-    </div>
+    </div>  
   );
 }
 
